2ee0e5371b4004a44d884c4fd42732dea2ca8c24
LAPTOP-795LNET2\kasie <KatarzynaKwasnik@simple.com.pl>
2023-06-01 11:54:54 +0200

    Adding Index Page and Controller

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller//TODO Wszystkie komponenty muszą być w tym samym pakiecie co klasa main!!!!!!!!
+public class IndexController {
+
+    @RequestMapping({"/", "", "/index", "/index.html"})
+    public String index() {
+
+        return "index";
+    }
+}
